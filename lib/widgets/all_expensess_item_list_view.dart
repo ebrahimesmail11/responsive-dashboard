6ba7dce6d2df495b4ea371facfb654998b8b0c06
ebrahim esmail <147981042+ebrahimesmail11@users.noreply.github.com>
2024-04-08 16:17:54 +0200

    Refactor AllExpensesItemListView to StatefulWidget
    Convert AllExpensesItemListView from StatelessWidget to StatefulWidget for better state management and interactivity. Add a method to update the selected index.

--- lib/widgets/all_expensess_item_list_view.dart
+++ lib/widgets/all_expensess_item_list_view.dart
@@ -4,27 +4,34 @@ import 'package:responsive_dash_board/widgets/all_expensess_item.dart';
 import '../models/all_expensess_item_model.dart';
 import '../utils/app_images.dart';
 
-class AllExpensesItemListView extends StatelessWidget {
+class AllExpensesItemListView extends StatefulWidget {
   const AllExpensesItemListView({super.key});
-static const items=[
-      AllExpensesItemModel(
+
+
+  @override
+  State<AllExpensesItemListView> createState() => _AllExpensesItemListViewState();
+}
+class _AllExpensesItemListViewState extends State<AllExpensesItemListView> {
+  final  items= [
+      const AllExpensesItemModel(
                   image: Assets.imagesBalance,
                   title: "Balance",
                   data: "April 2022",
                   price: "\$20,129"),
                     
-               AllExpensesItemModel(
+              const  AllExpensesItemModel(
                   image: Assets.imagesIncome,
                   title: "Income",
                   data: "April 2022",
                   price: "\$20,129"),
                   
-               AllExpensesItemModel(
+               const AllExpensesItemModel(
                   image: Assets.imagesExpenses,
                   title: "Expensesss",
                   data: "April 2022",
                   price: "\$20,129"),
 ];
+int isSelected=0;
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -33,15 +40,33 @@ static const items=[
       int index=e.key;
       var item=e.value;
       if(index==1){
-        return Expanded(child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 12.0),
-          child: AllExpensesItem(itemModel: item, isSelected: false,),
-        ));
+        return Expanded(
+          child: GestureDetector(
+            onTap: (){
+              updateIndex(index);
+            },
+            child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+            child: AllExpensesItem(itemModel: item, isSelected: isSelected==index,),
+          ),
+          ),
+        );
       }else{
-        return Expanded(child: AllExpensesItem(itemModel: item, isSelected: false,));
+        return Expanded(
+          child: GestureDetector(
+            onTap: () => updateIndex(index),
+            child: AllExpensesItem(itemModel: item, isSelected: isSelected==index,),
+          ),
+        );
       }
     }).toList(),
     );
     
+  }
+  
+  void updateIndex(int  index) {
+    setState(() {
+      isSelected=index;
+    });
   }
 }
