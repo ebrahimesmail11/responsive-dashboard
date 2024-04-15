ec32907c0dc91db8e460cfe1ea410590eed61960
ebrahim esmail <147981042+ebrahimesmail11@users.noreply.github.com>
2024-04-15 08:31:09 +0200

    Refactor LatestTransactionListView widget
    Update the layout to use SingleChildScrollView and Row for better performance
    and readability. Remove commented out ListView code.

--- lib/widgets/latest_transaction_list_view.dart
+++ lib/widgets/latest_transaction_list_view.dart
@@ -5,27 +5,46 @@ import 'package:responsive_dash_board/widgets/user_info_list_tile.dart';
 
 class LatestTransactionListView extends StatelessWidget {
   const LatestTransactionListView({super.key});
-static const  items = [
-  UserInfoModel(title: 'Madrani Andi', subTitle: 'Madraniadi20@gmail', image: Assets.imagesAvatar1),
-  UserInfoModel(title: 'Josua Nunito', subTitle: 'Josh Nunito@gmail.com', image: Assets.imagesAvatar2),
-  UserInfoModel(title: 'Madrani Andi', subTitle: 'Madraniadi20@gmail', image: Assets.imagesAvatar3),
-
-
-];
+  static const items = [
+    UserInfoModel(
+        title: 'Madrani Andi',
+        subTitle: 'Madraniadi20@gmail',
+        image: Assets.imagesAvatar1),
+    UserInfoModel(
+        title: 'Josua Nunito',
+        subTitle: 'Josh Nunito@gmail.com',
+        image: Assets.imagesAvatar2),
+    UserInfoModel(
+        title: 'Madrani Andi',
+        subTitle: 'Madraniadi20@gmail',
+        image: Assets.imagesAvatar3),
+  ];
   @override
   Widget build(BuildContext context) {
-    return  SizedBox(
-      height: 90,
-      child: ListView.builder(
-        itemCount: items.length,
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.only(top: 20.0),
-            child: IntrinsicWidth(child: UserInfoListTile(userInfoModel: items[index],)),
-          );
-        },
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: items
+            .map((e) => IntrinsicWidth(
+                child: UserInfoListTile(
+              userInfoModel: e,
+            )))
+            .toList(),
       ),
     );
+    // return  SizedBox(
+    //   height: 90,
+    //   child: ListView.builder(
+    //     itemCount: items.length,
+    //     scrollDirection: Axis.horizontal,
+    //     itemBuilder: (context, index) {
+    //       return Padding(
+    //         padding: const EdgeInsets.only(top: 20.0),
+    //         child: IntrinsicWidth(child: UserInfoListTile(userInfoModel: items[index],)),
+    //       );
+    //     },
+    //   ),
+    // );
   }
+  
 }
