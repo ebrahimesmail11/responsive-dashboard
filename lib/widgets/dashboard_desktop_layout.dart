bd7609da2138e8428cd62948a14ce7cd27b284b5
ebrahim esmail <147981042+ebrahimesmail11@users.noreply.github.com>
2024-04-24 20:28:31 +0200

    Add MyCard widget to dashboard desktop layout
    Add MyCard widget to the dashboard desktop layout for displaying
    information in a visually appealing card format.

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-import 'package:responsive_dash_board/widgets/all_expensess.dart';
 import 'package:responsive_dash_board/widgets/all_expensess_and_quich_invoice_section.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
-import 'package:responsive_dash_board/widgets/quich_invoice.dart';
+import 'package:responsive_dash_board/widgets/my_card.dart';
 
 class DashBoardDesktopLayout extends StatelessWidget {
   const DashBoardDesktopLayout({super.key});
@@ -23,6 +22,8 @@ class DashBoardDesktopLayout extends StatelessWidget {
           flex: 2,
           child: AllExpensessAndQuichInvoiceSection(),
         ),
+        SizedBox(width: 24,),
+        Expanded(child: MyCard(),),
       ],
     );
   }
