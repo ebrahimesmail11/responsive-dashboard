85bc478e4823d34a750aaccd53629205d0884f92
ebrahim esmail <147981042+ebrahimesmail11@users.noreply.github.com>
2024-07-28 16:23:52 +0300

    Refactor dashboard views to support stateful behavior and responsive drawer
    This commit introduces several enhancements to the dashboard views to make them stateful and more responsive, especially on mobile devices. The primary changes include:
    
    1. Conversion of `DashBoardViews` from a `StatelessWidget` to a `StatefulWidget` to allow for dynamic state management.
    2. Implementation of a `GlobalKey<ScaffoldState>` to control the Scaffold state, enabling the drawer to be opened programmatically.
    3. Addition of a `CustomDrawer` widget that is conditionally rendered based on the screen width, ensuring a responsive design.
    4. Adjustment of the `CustomDrawer` width to occupy 70% of the screen width on mobile devices for better usability.
    
    These changes aim to improve the user experience by making the dashboard more interactive and adaptable to different screen sizes.

--- lib/views/dashboard_views.dart
+++ lib/views/dashboard_views.dart
@@ -1,23 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/adaptive_layout_widget.dart';
+import 'package:responsive_dash_board/widgets/custom_drawer.dart';
 import 'package:responsive_dash_board/widgets/dashboard_desktop_layout.dart';
 import 'package:responsive_dash_board/widgets/dashboard_mobile_layout.dart';
 import 'package:responsive_dash_board/widgets/dashboard_tablet_layout.dart';
 
 
 
-class DashBoardViews extends StatelessWidget {
+class DashBoardViews extends StatefulWidget {
   const DashBoardViews({super.key});
 
   @override
+  State<DashBoardViews> createState() => _DashBoardViewsState();
+}
+
+class _DashBoardViewsState extends State<DashBoardViews> {
+  final GlobalKey<ScaffoldState> scaffoldKey=GlobalKey();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
       appBar: MediaQuery.of(context).size.width < 800 ? AppBar(
         backgroundColor:const  Color(0xFFFAFAFA),
         elevation: 0,
-        leading: const  Icon(Icons.menu),
+        leading:   IconButton(onPressed: (){
+          scaffoldKey.currentState!.openDrawer();
+        }, icon: const  Icon( Icons.menu),),
       ) : null,
       backgroundColor:const  Color(0xFFF7F9FA),
+      drawer: MediaQuery.of(context).size.width < 800 ? const  CustomDrawer():null,
       body: AdaptiveLayout(
         mobileLayout: (context) => const DashboardMobileLayout(),
         tabletLayout: (context) => const DashboardTabletLayout(),
